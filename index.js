3f2ffb4ed46527d3336f3f226f9be5bbd6b4a889
AnandIshmeet2009 <Ishmeetsinghanand14@gmail.com>
2022-08-26 19:44:56 +0530

    err solve

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express'); //link express to the file
 const app = express();
 const mongoose = require('mongoose'); //import mongoose
+const session = require('express-session'); //import session
 const authRoute = require('./routes/auth');  //authfile line
+const homeRoute = require('./routes/home'); //homefile linek
 require('dotenv').config(); //import dotenv
 
-app.use(express.static(__dirname + '/public')); //link to public folder
-app.set('view engine', 'ejs')   //set view engine to ejs
 
 //DB connect
 mongoose.connect(process.env.DB_URI,  
@@ -14,11 +14,27 @@ useUnifiedTopology : true
 }).then(() => console.log('DB connected!'))
 .catch(err => console.log(err));
 
+//Middlewares
+app.use(express.static(__dirname + '/public')); //link to public folder
+app.set('view engine', 'ejs') 
+app.use(
+    session({
+        secret : 'Ishmeet',
+        cookie : {
+            maxAge : 60000 * 60 * 24 // 1 day
+        },
+        saveUninitialized : true,
+        resave : false
+    })
+)
+
 //Routes
 app.use('/auth', authRoute);
+app.use ('/home', homeRoute);
+
 
 app.get('/', (req, res) =>{
-     res.render('hello'); 
+     res.render('index'); 
 })
 
 app.get('/about', (req, res) =>{ 
--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,97 @@
-const router = require('express').Router(); //link express to the file
-const axios = require('axios'); //import axios
-const urlParse = require('url-parse'); //import url-parse
-const qs = require('querystring'); // import querystring
-require('dotenv').config(); //make the file  to use .env
+const router = require('express').Router();
+const axios = require('axios');
+const urlParse = require('url-parse');
+const qs = require('query-string');
+const User = require('../models/User');
+require('dotenv').config()
 
-const redirectUri = process.env.REDIRECT_URI;
+const redirectUri = process.env.REDIRECT_URI
 
-
-function getGoogleAuthUrl() {
-    const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth?';
+function getGoogleAuthURL() {
+    const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
     const options = {
-        client_id: process.env.GOOGLE_CLIENT_ID,
         redirect_uri: redirectUri,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        access_type: 'offline',
         response_type: 'code',
         scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
-        access_type: 'offline',
         prompt: 'consent'
-    };
-    return `${rootUrl}${qs.stringify(options)}`;
+    }
 
+    return `${rootUrl}?${qs.stringify(options)}`
 }
-    router.get('/register', (req, res) => {
-    res.redirect(getGoogleAuthUrl());
-    res.send('Register Page')
+
+function getTokens({ code, clientId, clientSecret, redirectUri }) {
+    const url = 'https://oauth2.googleapis.com/token'
+    const options = {
+        code,
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+    }
+
+    return axios.post(url, qs.stringify(options), {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then((res) => res.data)
+        .catch(err => console.log(err))
+}
+
+
+
+router.get('/register', (req, res) => {
+    // creating consent screen url
+    res.redirect(getGoogleAuthURL())
 })
 
-
-router.get('/callback', (req, res) => {
+router.get('/callback', async (req, res) => {
     const queryUrl = new urlParse(req.url)
-    const data = qs.parse(queryUrl.query).code
+    const code = qs.parse(queryUrl.query).code
 
-    console.log(data)
+    // getting access tokens
+    const { id_token, access_token } = await getTokens({
+        code,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        redirectUri: redirectUri
+    })
+
+    // getting user data
+    const googleUser = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?alt=json&access_token=${access_token}`, {
+        headers: {
+            Authorization: `Bearer ${id_token}`
+        }
+    }).then(res => res.data)
+        .catch(err => console.log(err))
+
+    // saving user data to database
+    const doc = await User.findOne({ email: googleUser.email })
+    if (doc) {
+        //login
+        req.session.user = doc
+        res.redirect('/home')
+    } else {
+        //register
+
+        const newUser = new User({
+            email: googleUser.email,
+        })
+
+        newUser.save()
+            .then((resp) => {
+                req.session.user = resp
+                res.redirect('/home')
+            })
+            .catch(err => console.log(err))
+    }
 })
 
 
-// router.get('/auth/register', (req, res) => {
-//     res.send('Register Page')
-// })
 
 
 
-module.exports = router; 
+
+
+module.exports = router;
